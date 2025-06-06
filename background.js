50a7445bc0cbb1c9666ddbe21f478d5310948a19
Blake Varbai-Heward <104477413+AntalThomas@users.noreply.github.com>
2025-06-06 17:11:36 +1000

    Fix Chrome Extension Background Script Conflict

--- background.js
+++
@@ -1,6 +0,0 @@
-// Keeps tab open when clicking off the browser
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("src/entrypoints/popup/index.html")
-  });
-});
--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,3 +1,10 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
+  
+  // Handle extension icon click to open popup in new tab
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("src/entrypoints/popup/index.html")
+    });
+  });
 });
